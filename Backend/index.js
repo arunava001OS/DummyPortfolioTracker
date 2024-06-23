f29449d7fdf381907da93fc744ca3414fdc27036
arunava001OS <arunavasarkar3@gmail.com>
2024-06-23 17:47:14 +0530

    initial setup

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+//packages
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// custom modules
+const routes = require('./routes/routes');
+//DB Connection
+
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+// Server start
+
+const app = express();
+app.use('/api', routes)
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
